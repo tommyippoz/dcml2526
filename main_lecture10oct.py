60d3db66dd691d46b222e7bcde89538dcca51800
tommyippoz <tommaso.zoppi@unifi.it>
2025-10-10 15:03:32 +0200

    lecture 10 oct

---
+++ main_lecture10oct.py
@@ -0,0 +1,41 @@
+# REQUIREMENT: Monitor that reads free (RAM) memory every 1 second
+import csv
+import time
+from time import sleep
+import psutil
+
+
+def get_current_time_ms():
+    """
+    This is meant to return a timestamp in ms
+    :return: integer timestamp in ms
+    """
+    return int(time.time()*1000)
+
+
+stored_values = []
+i = 0
+while i < 5:
+    start_timestamp = get_current_time_ms()
+    print(start_timestamp)
+    mem_data_read = psutil.virtual_memory()
+    stored_values.append(mem_data_read)
+    print(mem_data_read.percent)
+    i = i + 1
+    before_sleep_timestamp = get_current_time_ms()
+    #sleep(1)
+    sleep((1000-(before_sleep_timestamp-start_timestamp))/1000)
+
+# open the file in the write mode
+with open('my_monitor_data.csv', 'w', newline='') as f_desc:
+    # create the csv writer
+    writer = csv.writer(f_desc)
+    # write a row to the csv file
+    writer.writerow(["total","available", "perc", "used", "free"])
+    for item in stored_values:
+        writer.writerow([item.total, item.available, item.percent, item.used, item.free])
+# This is the code we built in DCML lecture of Wednesday, Oct 8th.
+# Please note that this still has bugs and has to be updated
+
+
+
